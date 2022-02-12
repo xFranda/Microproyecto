661c903a2fc1050d17cb4fa709dbd72564bf99a0
Frandey Briceño <f.briceno@correo.unimet.edu.ve>
2022-02-12 13:06:22 -0400

    Proyectos sin links

--- main.js
+++ main.js
@@ -37,3 +37,36 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
+
+
+(function(){
+    
+    const sliders = [...document.querySelectorAll('.testimony__body')];
+    const buttonNext = document.querySelector('#next');
+    const buttonBefore = document.querySelector('#before');
+    let value;   
+
+    buttonNext.addEventListener('click', ()=>{
+        changePosition(1);
+    });
+
+    buttonBefore.addEventListener('click', ()=>{
+        changePosition(-1);
+    });
+
+    const changePosition = (add)=>{
+        const currentTestimony = document.querySelector('.testimony__body--show').dataset.id;
+        value = Number(currentTestimony);
+        value+= add;
+
+
+        sliders[Number(currentTestimony)-1].classList.remove('testimony__body--show');
+        if(value === sliders.length+1 || value === 0){
+            value = value === 0 ? sliders.length  : 1;
+        }
+
+        sliders[value-1].classList.add('testimony__body--show');
+
+    }
+
+})();
